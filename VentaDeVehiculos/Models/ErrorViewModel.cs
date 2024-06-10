57f75f8d69b31066702a5fab21c2fc603e84e764
Elias Rodriguez <elayasrodriguez@gmail.com>
2024-06-10 18:23:21 -0400

    Creacion del proyecto

---
+++ VentaDeVehiculos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VentaDeVehiculos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
