03b4cb335ad635c69f672d391c06a28d6d90a8ea
Elias Rodriguez <elayasrodriguez@gmail.com>
2024-06-10 19:14:42 -0400

    Migracion a la base de datos

---
+++ VentaDeVehiculos/Contexto/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VentaDeVehiculos.Models;
+
+namespace VentaDeVehiculos.Contexto
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+    }
+
+}
