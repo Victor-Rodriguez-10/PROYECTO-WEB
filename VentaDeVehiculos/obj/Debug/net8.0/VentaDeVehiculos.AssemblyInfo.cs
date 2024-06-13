68557794ce020d6dcc9a48047bae98d5c6551338
Elias Rodriguez <elayasrodriguez@gmail.com>
2024-06-12 23:26:36 -0400

    modifique el login

--- VentaDeVehiculos/obj/Debug/net8.0/VentaDeVehiculos.AssemblyInfo.cs
+++ VentaDeVehiculos/obj/Debug/net8.0/VentaDeVehiculos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92407c833b822a35d5f370fa760c6bf963d57aff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dd3767b38d5d122d82760799080a0f43793a876")]
 [assembly: System.Reflection.AssemblyProductAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
