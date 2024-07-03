c3739b9504309c3126a91804cb0b54e5763a4a45
Elias Rodriguez <elayasrodriguez@gmail.com>
2024-07-03 13:02:34 -0400

    Correccion general del proyecto

--- VentaDeVehiculos/obj/Debug/net8.0/VentaDeVehiculos.AssemblyInfo.cs
+++ VentaDeVehiculos/obj/Debug/net8.0/VentaDeVehiculos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382701532d1f186e985c0a296269e55f3eda056a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8561fc085055c4c32f051bd14ca292bf589e1f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
