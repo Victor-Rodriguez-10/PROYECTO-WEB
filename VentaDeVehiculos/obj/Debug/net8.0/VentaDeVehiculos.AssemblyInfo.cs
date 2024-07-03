12ce95af260621b7076e7f4cdb277bee6e78a5f3
Jonathan <05jonacf@gmail.com>
2024-07-03 01:43:37 -0300

    MODIFIQUE EL CATALOGO Y COPIE EL LOGIN

--- VentaDeVehiculos/obj/Debug/net8.0/VentaDeVehiculos.AssemblyInfo.cs
+++ VentaDeVehiculos/obj/Debug/net8.0/VentaDeVehiculos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3cf3368e3a11f59ef7f7825d8a919a18ac51771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa1c22858f051b608ee6cfb3b64dccaa3f7c6fc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
