f5b860180bb489ffd82aa290d5d8883fdcd773f2
Eito Enriquez <eitoenriquezarancibia.@gmail.com>
2024-07-02 23:20:06 -0400

    Correcion de login 2

--- VentaDeVehiculos/obj/Debug/net8.0/VentaDeVehiculos.AssemblyInfo.cs
+++ VentaDeVehiculos/obj/Debug/net8.0/VentaDeVehiculos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a8803b42f7157df2e44606539f3a19c683f864")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4440f184be5af5fd491244b64a0fad6c8971eb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VentaDeVehiculos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
